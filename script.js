5b00d28c51d3107338a4870b21bf6d8ff15621c6
Yash Raghubanshi <yashraghubanshi@Yashs-MacBook-Pro-3.local>
2025-02-11 23:14:41 +0530

    changes css

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ let mins = document.getElementById('mins');
 let sec = document.getElementById('sec');
 
 setInterval(() => {
-    let currentTime = new Date();
+    let currentTime =new Date();
     
-    hrs.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
-    min.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
-    sec.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
+    hrs.innerHTML =(currentTime.getHours()<10?"0":"") + currentTime.getHours();
+    min.innerHTML =(currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
+    sec.innerHTML =(currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
 }, 1000);
